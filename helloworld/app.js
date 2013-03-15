23c63dc1106f61bd6c6ba0163208aa05cdf2c796
Todd <todd@dreamfactory.com>
2013-03-15 14:34:58 -0400

    First

---
+++ helloworld/app.js
@@ -0,0 +1,15 @@
+function sayhello() {
+
+    $.ajax({
+        dataType: 'json',
+        url: location.protocol + '//' + location.host + '/rest/User/Profile',
+        data: 'method=GET&app_name=helloworld',
+        cache: false,
+        success: function (response) {
+            $('#demo').html('Hello, ' + response.display_name + '!!!');
+        },
+        error: function (response) {
+            alert("There was an error retrieving the user profile.");
+        }
+    });
+}
